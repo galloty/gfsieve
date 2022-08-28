1af81b94f144ad774096bc1d5f47f4edbd28c5f9
Yves Gallot <59421144+galloty@users.noreply.github.com>
2022-08-28 14:48:48 +0200

    static build

--- gfsieveCPU/build.cmd
+++ gfsieveCPU/build.cmd
@@ -1,3 +1,3 @@
 @setlocal
-@set path=C:\msys64\mingw64\bin
-g++ -m64 -O3 src/gfsieveCPU.cpp -o gfsieveCPU.exe
+set path=C:\msys64\mingw64\bin
+g++ -m64 -O3 -static src/gfsieveCPU.cpp -o gfsieveCPU.exe
